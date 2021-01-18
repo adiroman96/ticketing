258e678fa6b96f1ae71debcedc9d3d9fa7421855
Adrian Roman <58778585+adiroman96@users.noreply.github.com>
2021-01-18 19:54:06 +0200

    auth using common package

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -4,8 +4,8 @@ import jwt from 'jsonwebtoken';
 
 import { Password } from '../services/password';
 import { User } from '../models/user';
-import { validateRequest } from '../middlewares/validate-request';
-import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '@radix-soft/common'
+import { BadRequestError } from '@radix-soft/common'
 
 const router = express.Router();
 
