a62a8bd1eee6e7df5fb0e6afc7122c8678984d6f
Adrian Roman <58778585+adiroman96@users.noreply.github.com>
2021-01-19 22:44:10 +0200

    get add update TICKET

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -5,31 +5,28 @@ import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, [
-  body('title')
-  .not()
-  .isEmpty() // title is not provided or title === ''
-  .withMessage('Title is required'),
+router.post(
+  '/api/tickets',
+  requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be greater than 0'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
 
-  body('price')
-  .isFloat({ gt: 0 })
-  .withMessage('Price must be greater than 0')
-], 
-validateRequest,
-async (req: Request, res: Response) => {
+    const ticket = Ticket.build({
+      title,
+      price,
+      userId: req.currentUser!.id,
+    });
+    await ticket.save();
 
-  // taking and saving data
-  const {title, price} = req.body;
-
-  const ticket = Ticket.build({
-    title, 
-    price,
-    userId: req.currentUser!.id
-  })
-
-  await ticket.save();
-
-  res.status(201).send(ticket);
-});
+    res.status(201).send(ticket);
+  }
+);
 
 export { router as createTicketRouter };
