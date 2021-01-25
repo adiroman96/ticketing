726256eca442ad1f0e0c67e550fc877e8c365e89
Adrian Roman <58778585+adiroman96@users.noreply.github.com>
2021-01-25 10:00:29 +0200

    orders handles expiration

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -14,7 +14,7 @@ import { OrderCreatedPublisher } from '../events/publishers/order-created-publis
 import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
-const EXPIRATION_WINDOW_SECONDS = 1 * 60; //expiration time in minutes
+const EXPIRATION_WINDOW_SECONDS = 15; 
 
 router.post(
   '/api/orders',
